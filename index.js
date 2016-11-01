57b86e9adddaf3ecc5f2dcf548397e211dab3225
Dombi Soma Kristóf <dombis@sonrisa.hu>
2016-11-01 12:23:11 +0100

    Initial commit ( . Y . ) love boobies

---
+++ index.js
@@ -0,0 +1,12 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var port = 80;
+
+server.listen(port, function() {
+	console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
